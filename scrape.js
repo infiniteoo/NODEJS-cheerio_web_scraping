c991e87192862e52cccd53dbe7faafe871d09cc5
Troy Dorman <troydorman@gmail.com>
2021-05-03 12:40:46 -0600

    changed the website url i used to a fake one so no drama;

--- scrape.js
+++ scrape.js
@@ -1,11 +1,32 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.csv');
 
-request('http://codedemos.com/sampleblog', (error, response, html) => {
+// Write Headers
+writeStream.write(`Title,Link,Date \n`);
 
-    if(!error && response.statusCode === 200) {
-        const $ = cheerio.load(html);
-        
-    } 
+request('https://samplewebsite_blahblah.com/', (error, response, html) => {
+  if (!error && response.statusCode == 200) {
+    const $ = cheerio.load(html);
 
-})
+    $('.content').each((i, el) => {
+      const title = $(el)
+        .find('.title')
+        .text()
+        .replace(/\s\s+/g, '');
+      const link = $(el)
+        .find('a')
+        .attr('href');
+      const date = $(el)
+        .find('.excerpt')
+        .text()
+        .replace(/,/, '');
+
+      // Write Row To CSV
+      writeStream.write(`${title}, ${link}, ${date} \n`);
+    });
+
+    console.log('Scraping Done...');
+  }
+});
