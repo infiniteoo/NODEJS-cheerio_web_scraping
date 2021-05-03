3271c557fb168e7450dc61b45aafd667c3594ad8
Troy Dorman <troydorman@gmail.com>
2021-05-03 12:18:46 -0600

    initialized the npm package and installed dependencies.  made request

---
+++ scrape.js
@@ -0,0 +1,11 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+request('http://codedemos.com/sampleblog', (error, response, html) => {
+
+    if(!error && response.statusCode === 200) {
+        const $ = cheerio.load(html);
+        
+    } 
+
+})
